332d1113c196bc49d3bbb7f43e002a6ad67fd07a
Steve <ostylehouse@gmail.com>
2022-11-17 17:21:09 +1300

    ricketily EditorView-ing

--- src/routes/Code.svelte
+++ src/routes/Code.svelte
@@ -1,27 +1,41 @@
 <script lang="ts">
+	import { onMount } from 'svelte'
     import {parser} from '../lang/style.js'
+    let b = ':3'
 
-    let f = (x) => ":"+x
-    declare function f(x: number): string;
-    let x: number | null | undefined = null
-    if (x) {
-    f(x); // Ok, type of x is number here
-    } else {
-    f(x); // Error, type of x is number? here
-    }
-    let a = x != null ? f(x) : ""; // Type of a is string
-    let b = x && f(x); // Type of b is string | 0 | null | undefined
+    // lezer
+        let flub = "i thing/with/etc\n\no yeses/because\n"
+        let tree = parser.parse(flub)
+        let cursor = tree.cursor()
+        let lems = []
+        while (cursor.next()) {
+            lems.push(`Node ${cursor.name} from ${cursor.from} to ${cursor.to}`)
+        }
 
-    let flub = "i thing/with/etc\n\no yeses/because\n"
-    let tree = parser.parse(flub)
-    let cursor = tree.cursor()
-    let lems = []
-    while (cursor.next()) {
-        lems.push(`Node ${cursor.name} from ${cursor.from} to ${cursor.to}`)
-    } 
+    // EditorView
+        import {EditorState} from "@codemirror/state"
+        import {EditorView, keymap} from "@codemirror/view"
+        import {defaultKeymap} from "@codemirror/commands"
+
+        let startState = EditorState.create({
+            doc: flub,
+            extensions: [keymap.of(defaultKeymap)]
+        })
+        
+        let ele
+        let view
+        onMount(() => {
+            view = new EditorView({
+                state: startState,
+                parent: ele
+            })
+        })
 </script>
+
+
 <p> {b} </p>
-<ul>
+<div bind:this={ele}></div>
+<ul >
     {#each lems as lem}
         <li> {lem} </li>
     {/each}
