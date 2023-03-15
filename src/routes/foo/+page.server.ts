329097d61a706d26da5a3264a9774ad56373bf33
Steve <ostylehouse@gmail.com>
2023-03-15 15:31:08 +1300

    with page param

--- src/routes/foo/+page.server.ts
+++ src/routes/foo/+page.server.ts
@@ -1,5 +1,6 @@
 import { execSync } from 'child_process';
 
+let page = 99
 function pdfToPng(pdfPath, pageNum) {
     const pngPath = `${pdfPath}-page-${pageNum}.png`;
     const command = `convert -density 150 "${pdfPath}[${pageNum - 1}]" "static/${pngPath}"`;
@@ -9,6 +10,13 @@ function pdfToPng(pdfPath, pageNum) {
 
 export function load() {
 	return {
-        png: pdfToPng('abop.pdf', 99)
+        png: pdfToPng('abop.pdf', page),
+        page
     }
 }
+export const actions = {
+	default: async ({ cookies, request }) => {
+		const data = await request.formData();
+		page = data.get('page')
+	}
+};
--- src/routes/foo/+page.svelte
+++ src/routes/foo/+page.svelte
@@ -1,4 +1,9 @@
 <script>
 	export let data;
 </script>
-<h2>pdf<img src={data.png} class=inscreen alt="Page 99 of my PDF file" /></h2>
+<h2>pdf<form method="POST">
+	<label>
+		the page:
+		<input name="page" value={data.page} />
+	</label>
+</form><img src={data.png} class=inscreen alt="Page 99 of my PDF file" /></h2>
