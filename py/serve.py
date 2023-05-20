bd0a0e27019557a49af7280d247bb82714ebeaee
Steve <ostylehouse@gmail.com>
2023-05-20 18:13:27 +1200

    serve.py: directory but not thumbnails

--- py/serve.py
+++ py/serve.py
@@ -2,23 +2,42 @@ import os
 from flask import Flask, jsonify, send_from_directory
 
 app = Flask(__name__)
+app.debug = True  # Enable verbose mode
+
+thumbdir = 'static/thumb'
 
 @app.route('/thumbnails/<path:filename>')
 def thumbnails(filename):
-    thumbnail_path = os.path.join('thumbnails', filename)
-    return send_from_directory('static/thumb', thumbnail_path)
+    directory, filename = os.path.split(filename)
+    place = os.path.join(thumbdir,directory)
+    print("Looking for "+place+" + "+filename)
+    if os.path.isfile(os.path.join(place, filename)):
+        print (" Existo!")
+    return send_from_directory(place, filename)
 
-@app.route('/directory')
-def directory():
+@app.route('/directory/', defaults={'path': ''})
+@app.route('/directory/<path:path>')
+def directory(path):
     directory_path = '/v'
+    if path:
+        directory_path = os.path.join(directory_path, path)
+    # may request a file
+    if os.path.isfile(directory_path):
+        directory, filename = os.path.split(directory_path)
+        return send_from_directory(directory, filename)
+    
     file_list = []
     for filename in os.listdir(directory_path):
-        if filename.endswith(('.jpg', '.jpeg', '.png', '.gif', '.mp4')):
-            file_info = {
-                'name': filename,
-                'thumbnail_url': f"/thumbnails/{filename}"
-            }
-        file_list.append(filename)
+        f = {'f':filename}
+        if os.path.isdir(os.path.join(directory_path, filename)):
+            # is dir
+            f['d'] = 1
+        else:
+            if (os.path.isfile(os.path.join(thumbdir, filename))):
+                # has thumbnail already
+                f['t'] = 1
+        
+        file_list.append(f)
     
     return jsonify(file_list)
 
